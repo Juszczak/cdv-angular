dc46628f69fda316fa26b6beea47a5d3514f40c6
Adrian Juszczak <adrian@juszczak.io>
2019-06-08 16:28:23 +0200

    contact form
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,43 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent,
+    AboutComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"]
 })
 export class ContactComponent implements OnInit {
+  public name: string;
+  public email: string;
+  public message: string;
 
-  constructor() { }
+  public isEmailInvalid: boolean;
+  public isNameInvalid: boolean;
 
-  ngOnInit() {
+  constructor() {
+    this.name = "";
+    this.email = "";
+    this.message = "";
   }
 
+  ngOnInit() {}
+
+  public emailChanged() {
+    this.validateEmail();
+  }
+
+  public formSubmit(): void {
+    this.validateEmail();
+    this.validateName();
+  }
+
+  private validateEmail(): void {
+  if (this.email.length > 0 && /[a-z]@[a-z]/.test(this.email)) {
+      this.isEmailInvalid = false;
+    } else {
+      this.isEmailInvalid = true;
+    }
+  }
+
+  private validateName(): void {
+    if (this.name.length > 0) {
+      this.isNameInvalid = false;
+    } else {
+      this.isNameInvalid = true;
+    }
+  }
 }
