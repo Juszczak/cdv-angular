6c29c9941a8fc70d16fad739e9fd0717af1cd16f
Adrian Juszczak <adrian@juszczak.io>
2019-06-08 13:45:58 +0200

    empty contact component
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
