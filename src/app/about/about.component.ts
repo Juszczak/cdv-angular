f0e3aa92351e8bf016e0d57312fa94dff86a5dce
Adrian Juszczak <adrian@juszczak.io>
2019-06-09 16:07:38 +0200

    swapi
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Planet {
+  name: string;
+  terrain: string;
+  climate: string;
+  url: string;
+}
+
+interface SwapiResponse {
+  count: number;
+  next: string;
+  previous: string;
+  results: Planet[];
+}
 
 @Component({
   selector: 'app-about',
@@ -6,10 +21,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public planets: Planet[];
+  private http: HttpClient;
+  private previous: string;
+  private next: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(http: HttpClient) {
+    this.http = http;
   }
 
+  public ngOnInit(): void {
+    const url = 'https://swapi.co/api/planets/?format=json';
+    this.getPage(url);
+  }
+
+  private getPage(url: string): void {
+    const response = this.http.get(url);
+    response.subscribe((data: SwapiResponse) => {
+      const planets: Planet[] = data.results;
+      this.planets = planets;
+      this.next = data.next;
+      this.previous = data.previous;
+    });
+  }
+
+  public previousPage(): void {
+    this.getPage(this.previous);
+  }
+
+  public nextPage(): void {
+    this.getPage(this.next);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
@@ -45,7 +46,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
